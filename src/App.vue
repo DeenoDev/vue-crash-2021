325c14f58a35003b935978f5781059c458097cc7
DeenoDev <deyanoklaasen@outlook.com>
2024-01-10 23:53:46 +0200

    added props to header.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-  <Header />
+  <Header title="Hello" />
 </div>
 </template>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
     <header>
-        <h1>Task Tracker</h1>
+        <h1>{{title}}</h1>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'App',
-    components: {},
+    name: 'Header',
+    props: {
+        title: String,
+    },
 }
 
 </script>
