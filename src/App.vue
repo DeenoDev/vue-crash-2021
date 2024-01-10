bb303604a8312544869f6178019f776c1f7991ca
DeenoDev <deyanoklaasen@outlook.com>
2024-01-10 23:47:23 +0200

    imported header.vue component into app.vue file.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
-  <h1>Hello World</h1>
+  <Header />
 </div>
 </template>
 
 <script>
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
