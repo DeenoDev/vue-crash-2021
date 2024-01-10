47780fb08e6ea036a5320bea12209f2cd75e1b8c
DeenoDev <deyanoklaasen@outlook.com>
2024-01-11 00:07:10 +0200

    finally added dynamic props to button.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,13 @@
 <template>
-    <button class="btn">Add Task</button>
+    <button :style="{background: color}" class="btn">{{text}}</button>
 </template>
 
 <script>
   export default {
     name: 'Button',
+    props: {
+        text: String,
+        color: String
+    }
   }
 </script>
