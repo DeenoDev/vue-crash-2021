6c8bf65645cb196516f4d874d8b81a83e75ae15f
DeenoDev <deyanoklaasen@outlook.com>
2024-01-10 23:40:31 +0200

    Added template and script for Header.vue.

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+}
+
+</script>
+
+<style>
+   @import url();
+</style>
