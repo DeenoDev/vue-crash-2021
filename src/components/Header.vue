273b37f6444db66a5cbaa503506f40ca1b2daa9c
DeenoDev <deyanoklaasen@outlook.com>
2024-01-11 23:04:04 +0200

    Added 2 new buttonsusing the Button component with different props.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,8 @@
     <header>
         <h1>{{title}}</h1>
         <Button text="Add Task" color="green" />
+        <Button text="Update Task" color="blue" />
+        <Button text="Delete Task" color="red" />
     </header>
 </template>
 
