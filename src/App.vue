2766436ee93ec525de1c7b28a384d363076434ee
DeenoDev <deyanoklaasen@outlook.com>
2024-01-11 23:21:04 +0200

    Used the created lifecycle to define tasks and added hard coded array of task objects.

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,30 @@ export default {
   },
   data(){
     return{
-      tasks:
+      tasks: []
     }
+  }, 
+  created(){
+    this.task = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 3rd at 11:00am',
+        reminder: false,
+      },
+    ]
   }
 }
 </script>
