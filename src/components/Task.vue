682d0ad17ab831171d42cc7a60401853478f87fb
DeenoDev <deyanoklaasen@outlook.com>
2024-01-15 23:01:14 +0200

    moved styling from tasks.vue file to task.vue file.

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -14,3 +14,26 @@ export default {
 }
 
 </script>
+
+<style scope>
+.fas {
+  color: red;
+}
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
