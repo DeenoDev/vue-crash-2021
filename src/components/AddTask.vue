f6d53d6adda1bc8770744e95e699ec52b7adfe3e
DeenoDev <deyanoklaasen@outlook.com>
2024-01-18 23:22:35 +0200

    Added script tags in AddTask.vue and exported default.

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,10 +1,10 @@
 <template>
      <form @submit="onSubmit" class="add-form">
-    <div class="form-control">
+     <div class="form-control">
       <label>Task</label>
       <input type="text" v-model="text" name="text" placeholder="Add Task" />
-    </div>
-    <div class="form-control">
+     </div>
+     <div class="form-control">
       <label>Day & Time</label>
       <input
         type="text"
@@ -12,12 +12,18 @@
         name="day"
         placeholder="Add Day & Time"
       />
-    </div>
-    <div class="form-control form-control-check">
+     </div>
+     <div class="form-control form-control-check">
       <label>Set Reminder</label>
       <input type="checkbox" v-model="reminder" name="reminder" />
-    </div>
+     </div>
 
     <input type="submit" value="Save Task" class="btn btn-block" />
   </form>
 </template>
+
+<script>
+ export default {
+    name: 'AddTask',
+ }
+</script>
