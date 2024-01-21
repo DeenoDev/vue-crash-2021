494da97a310c8dcd5b0299b29cf7f3d5d3ea51a4
DeenoDev <deyanoklaasen@outlook.com>
2024-01-21 22:33:31 +0200

    Added red button state when form is displayed.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{title}}</h1>
-        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" color="green" />
+        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'" />
     </header>
 </template>
 
