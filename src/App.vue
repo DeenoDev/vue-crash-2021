8b4d0c461d79a3e246b5b686ca28919ed647a732
DeenoDev <deyanoklaasen@outlook.com>
2024-01-23 23:56:10 +0200

    removed hard coded task data.

--- src/App.vue
+++ src/App.vue
@@ -54,27 +54,7 @@ export default {
     }
   },
   created(){
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'March 3rd at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'March 3rd at 11:00am',
-        reminder: false,
-      },
-    ]
-  }
+  },
 }
 </script>
 
