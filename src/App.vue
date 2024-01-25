bd7b7071c1425edc292b7dec60c30d2651769cad
DeenoDev <deyanoklaasen@outlook.com>
2024-01-25 22:58:42 +0200

    fetchTask function created. this function takes in ID as an argument and returns a single piece of data.

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,13 @@ export default {
         return data
     }
   },
+  async fetchTasks(id){
+        const res = await fetch(`http://localhost:5000/tasks/${id}`)
+
+        const data = await res.json()
+
+        return data
+    },
   async created(){
     this.tasks = await this.fetchTasks();
   },
