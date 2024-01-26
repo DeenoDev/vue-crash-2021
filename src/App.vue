4b6a3eb3eaf462c395bd26d0f4b22e7b501ad234
DeenoDev <deyanoklaasen@outlook.com>
2024-01-26 23:27:26 +0200

    created an async function with the deleteTask function within app.vue file.

--- src/App.vue
+++ src/App.vue
@@ -45,9 +45,17 @@ export default {
       this.tasks = [...this.tasks, data]
 
     },
-    deleteTask(id){
+    async deleteTask(id){
       if (confirm('Are you sure?')){
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const res = await fetch(`/api/tasks/${id}`,
+        {
+          method: 'DELETE'
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+        
+
+        
       }
     },
     toggleReminder(id){
