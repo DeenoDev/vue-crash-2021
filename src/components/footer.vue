2bcca79bfcfe9feeaedd35c15e01c45a138a3cdf
DeenoDev <deyanoklaasen@outlook.com>
2024-01-27 22:15:47 +0200

    added basic styling to footer section.

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,17 @@
 <template>
     <footer>
         <p>Copyright &copy; 2024</p>
+        <a href="/about">About</a>
     </footer>
 </template>
+
+<style scoped>
+  a {
+    color: #333;
+  }
+
+  footer {
+    margin-top: 30px;
+    text-align: center;
+  }
+</style>
