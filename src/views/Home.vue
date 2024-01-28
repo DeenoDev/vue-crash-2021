372a5e58b78f434a943ea09dbad705f55109cbef
DeenoDev <deyanoklaasen@outlook.com>
2024-01-28 23:37:16 +0200

    v-show directive moved into AddTask component props and removed the encapsulated div.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
-    <div v-show="showAddTask" >
-    <AddTask @add-task="addTask"/>
-  </div>
-  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <AddTask v-show="showAddTask" @add-task="addTask"/>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
 </template>
 
 <script>
