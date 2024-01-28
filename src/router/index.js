815e8d55c300ef05c830a3d5da87a077bd496180
DeenoDev <deyanoklaasen@outlook.com>
2024-01-28 22:55:30 +0200

    created Home.vue file inside views folder and imported Home.vue into index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home'
 import About from '../views/About'
 
 const routes = [
@@ -6,7 +7,7 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home,
-    }
+    },
     {
         path: '/about',
         name: 'about',
