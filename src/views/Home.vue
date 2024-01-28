f29b21743d9fb96413d4a94ccf13b5f4fe1e0b27
DeenoDev <deyanoklaasen@outlook.com>
2024-01-28 22:59:35 +0200

    cut out the AddTask & Tasks component from App.vue file and pasted it into Home.vue file with script to import the components.

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-show="showAddTask" >
+    <AddTask @add-task="addTask"/>
+  </div>
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+</template>
+
+<script>
+  import Tasks from './components/Tasks';
+  import AddTask from './components/AddTask';
+</script>
