6c90263170325934821387b2684ea16f98b197fa
DeenoDev <deyanoklaasen@outlook.com>
2024-01-28 22:43:32 +0200

    fixed router-link bug in Footer.vue by changing syntax error.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <AddTask @add-task="addTask"/>
   </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  <router-view></router-view>
 </div>
 <Footer />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
         component: About
     }
     
-]
+];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
