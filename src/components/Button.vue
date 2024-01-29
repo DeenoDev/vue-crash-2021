a299ae17b38858a14b15612e49f91412986b9d32
DeenoDev <deyanoklaasen@outlook.com>
2024-01-29 21:35:43 +0200

    Button.vue file updated.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,18 +1,20 @@
 <template>
-    <button @click="onClick()" :style="{background: color}" class="btn">{{text}}</button>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
 </template>
 
 <script>
-  export default {
-    name: 'Button',
-    props: {
-        text: String,
-        color: String
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit('btn-click')
     },
-    methods: {
-      onClick(){
-        this.$emit('btn-click')
-      }
-    }
-  }
+  },
+}
 </script>
