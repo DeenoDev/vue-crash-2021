ffdfb2d06439136c21ce72047058bd0f36ef7b99
DeenoDev <deyanoklaasen@outlook.com>
2024-01-29 21:37:11 +0200

    Header.vue file updated.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,44 @@
 <template>
     <header>
-        <h1>{{title}}</h1>
-        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'" />
+      <h1>{{ title }}</h1>
+      <Button
+        v-show="homePage"
+        @btn-click="$emit('toggle-add-task')"
+        :text="showAddTask ? 'Close' : 'Add Task'"
+        :color="showAddTask ? 'red' : 'green'"
+      />
     </header>
-</template>
-
-<script>
-import Button from './Button';
-
-
-
-export default {
+  </template>
+  
+  <script>
+  import Button from './Button'
+  
+  export default {
     name: 'Header',
     props: {
-        title: String,
-        showAddTask: Boolean
+      title: String,
+      showAddTask: Boolean,
     },
     components: {
-        Button,
+      Button,
     },
     computed: {
-        homePage(){
-            if(this.$route.path === '/'){
-                return true
-            } else {
-                return false
-            }
+      homePage() {
+        if (this.$route.path === '/') {
+          return true
+        } else {
+          return false
         }
-    }
-}
-
-</script>
-
-<style scoped>
-   header {
+      },
+    },
+  }
+  </script>
+  
+  <style scoped>
+  header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 20px;
-   }
-</style>
+  }
+  </style>
